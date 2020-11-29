850c4148ba8d129f639e02bbc186ed8a347f574d
杨厚皓 <yanghouhao@bytedance.com>
2020-11-29 23:18:41 +0800

    headerfile

---
+++ model/myBlock.cpp
@@ -0,0 +1,50 @@
+#include "myBlock.h"
+
+myBlock::myBlock(time_t timeStamp,
+             std::string prevBlockHash,
+             int height,
+             std::vector<myTransaction *> transactionsVector)
+{
+    this->timeStamp = timeStamp;
+    this->prevBlockHash = prevBlockHash;
+    this->height = height;
+    this->transactionsVector = transactionsVector;
+    this->caculateHash();
+}
+
+myBlock::~myBlock()
+{
+}
+
+time_t myBlock::getTimeStamp() const
+{
+    return this->timeStamp;
+}
+
+size_t myBlock::getBlockHash() const
+{
+    return this->hash;
+}
+
+std::string myBlock::getPrevBlockHash() const
+{
+    return this->prevBlockHash;
+}
+
+int myBlock::getHeight() const
+{
+    return this->height;
+}
+
+std::vector<myTransaction *> myBlock::getTransactionsVector() const
+{
+    return this->transactionsVector;
+}
+
+void myBlock::caculateHash()
+{
+    size_t timeStamp = std::hash<time_t>()(this->timeStamp);
+    size_t prevBlockHash = std::hash<std::string>()(this->prevBlockHash);
+    size_t height = std::hash<int>()(this->height);
+    this->hash = timeStamp ^ prevBlockHash ^ height;
+}
---
+++ model/myTransaction.cpp
@@ -0,0 +1,49 @@
+#include "myTransaction.h"
+
+myTransaction::myTransaction(JSDescription *jsDescription, string cipherTextWithABE)
+{
+    this->jsDescription = new JSDescription;
+    this->jsDescription->vpub_old = jsDescription->vpub_old ;
+    this->jsDescription->vpub_new = jsDescription->vpub_new ;
+    this->jsDescription->anchor = jsDescription->anchor ;
+    this->jsDescription->nullifiers = jsDescription->nullifiers ;
+    this->jsDescription->commitments = jsDescription->commitments ;
+    this->jsDescription->ephemeralKey = jsDescription->ephemeralKey ;
+    this->jsDescription->ciphertexts = jsDescription->ciphertexts ;
+    this->jsDescription->randomSeed = jsDescription->randomSeed ;
+    this->jsDescription->macs = jsDescription->macs ;
+    this->jsDescription->proof = jsDescription->proof;
+
+    this->cipherTextWithABE = cipherTextWithABE;
+    this->transactionHash = std::hash<string>()(this->cipherTextWithABE);
+}
+
+myTransaction::~myTransaction()
+{
+    delete this->jsDescription;
+}
+
+size_t myTransaction::getTransactionHash() const
+{
+    return this->transactionHash;
+}
+
+JSDescription myTransaction::makeSproutProof(
+        const std::array<JSInput, 2>& inputs,
+        const std::array<JSOutput, 2>& outputs,
+        const Ed25519VerificationKey& joinSplitPubKey,
+        uint64_t vpub_old,
+        uint64_t vpub_new,
+        const uint256& rt
+){
+    return JSDescription(joinSplitPubKey, rt, inputs, outputs, vpub_old, vpub_new);
+}
+
+bool myTransaction::verifySproutProof(
+        const JSDescription& jsdesc,
+        const Ed25519VerificationKey& joinSplitPubKey
+)
+{
+    auto verifier = ProofVerifier::Strict();
+    return verifier.VerifySprout(jsdesc, joinSplitPubKey);
+}
